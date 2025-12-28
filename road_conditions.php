09a0dc3576df9a7007d29f85a952de297d12ea63
MagnamPH <creedservio@gmail.com>
2025-12-28 15:47:15 +0800

    Update road_conditions.php

--- road_conditions.php
+++ road_conditions.php
@@ -51,6 +51,11 @@
           <i class="fas fa-ticket-alt"></i>
           <span>Violation Ticketing</span>
         </a>
+
+                <a href="#" class="sidebar-link">
+          <i class="fas fa-ticket-alt"></i>
+          <span>Violation Ticketing</span>
+        </a>
       </div>
 
     </nav>
